33a9ab8995221f74ff3ba5fb77d75f61e910a665
박초롱 <rong9835@naver.com>
2024-11-25 20:41:38 +0900

    init:shadcn ui+Next.js 프로젝트 초기설정

---
+++ app/layout.tsx
@@ -0,0 +1,27 @@
+import type { Metadata } from "next";
+import { Toaster } from "@/components/ui/toast/toaster";
+/** 폰트 */
+import { FONT_NOTOSANSKR } from "@/public/assets/fonts";
+/** 스타일 */
+import "@/public/styles/globals.css";
+import "@/public/styles/main.scss";
+
+export const metadata: Metadata = {
+  title: "TODO-BOARD 만들기",
+  description: "Shadcn UI 및 Supabase를 활용한 나만의 TODO-BOARD 만들기",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="ko">
+      <body className={FONT_NOTOSANSKR.className}>
+        {children}
+        <Toaster />
+      </body>
+    </html>
+  );
+}
